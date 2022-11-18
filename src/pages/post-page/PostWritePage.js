eedf54ebeb5dc859bd767d6e4588501b8836638e
choheejin <hjcho9510@gmail.com>
2022-11-18 11:28:33 +0900

    구조 변경 및 포스트 디테일 완료

---
+++ src/pages/post-page/PostWritePage.js
@@ -0,0 +1,8 @@
+export default function PostWritePage() {
+    return(
+        <div className="w-full flex justify-center items-center">
+            <div className="w-[80%] flex flex-col gap-8">
+            </div>
+        </div>
+    );
+}
