c2e765847278b6feb521ca0c3a513c9c29d56b82
yewon0804 <ehdnjs0804@gmail.com>
2022-11-27 05:07:22 +0900

    일부 페이지에 애니메이션 추가
    PostDetailPage
    - 페이지 입장 시, fade in 효과
    - 이미지 reload icon에 pulse, hover:spin
    
    기타 hover 추가

--- src/pages/post-page/PostDetailPage.js
+++ src/pages/post-page/PostDetailPage.js
@@ -1,6 +1,7 @@
-import {useParams} from "react-router-dom";
-import {useState} from "react";
-import {CommentItem, PostItem, PostWriter} from "./components";
+import '../../customAnimation/fadein.css';
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { CommentItem, PostItem, PostWriter } from "./components";
 
 export default function PostDetailPage() {
     const [pressLike, setPressLike] = useState(true);
@@ -37,7 +38,7 @@ export default function PostDetailPage() {
 
 
     return (
-        <div className="w-full flex justify-center items-center">
+        <div className="w-full flex justify-center items-center fadein">
             <div className="w-[60%] flex flex-col gap-8">
 
                 {/*글내용*/}
@@ -52,7 +53,7 @@ export default function PostDetailPage() {
                     <div className="flex flex-col gap-2">
                         {
                             data.split("\n").map((txt) => (
-                                <div>{txt}<br/></div>
+                                <div>{txt}<br /></div>
                             ))
                         }
                     </div>
@@ -64,19 +65,19 @@ export default function PostDetailPage() {
                         setPressLike(!pressLike);
                         console.log(pressLike)
                     }}
-                         className={`${pressLike === true ? 'bg-blue-500 text-white border-none drop-shadow' : ''} flex gap-0.5 rounded-full justify-center items-center border-2 border-gray-600 mb-10 w-14 h-14`}>
+                        className={`${pressLike === true ? 'bg-blue-500 text-white border-none drop-shadow' : ''} flex gap-0.5 rounded-full justify-center items-center border-2 border-gray-600 mb-10 w-14 h-14`}>
                         {
                             pressLike === true ?
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white"
-                                     className="w-5 h-5">
+                                    className="w-5 h-5">
                                     <path
-                                        d="M7.493 18.75c-.425 0-.82-.236-.975-.632A7.48 7.48 0 016 15.375c0-1.75.599-3.358 1.602-4.634.151-.192.373-.309.6-.397.473-.183.89-.514 1.212-.924a9.042 9.042 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75 2.25 2.25 0 012.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H14.23c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23h-.777zM2.331 10.977a11.969 11.969 0 00-.831 4.398 12 12 0 00.52 3.507c.26.85 1.084 1.368 1.973 1.368H4.9c.445 0 .72-.498.523-.898a8.963 8.963 0 01-.924-3.977c0-1.708.476-3.305 1.302-4.666.245-.403-.028-.959-.5-.959H4.25c-.832 0-1.612.453-1.918 1.227z"/>
+                                        d="M7.493 18.75c-.425 0-.82-.236-.975-.632A7.48 7.48 0 016 15.375c0-1.75.599-3.358 1.602-4.634.151-.192.373-.309.6-.397.473-.183.89-.514 1.212-.924a9.042 9.042 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75 2.25 2.25 0 012.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H14.23c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23h-.777zM2.331 10.977a11.969 11.969 0 00-.831 4.398 12 12 0 00.52 3.507c.26.85 1.084 1.368 1.973 1.368H4.9c.445 0 .72-.498.523-.898a8.963 8.963 0 01-.924-3.977c0-1.708.476-3.305 1.302-4.666.245-.403-.028-.959-.5-.959H4.25c-.832 0-1.612.453-1.918 1.227z" />
                                 </svg>
                                 :
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
-                                     stroke="currentColor" className="w-5 h-5">
+                                    stroke="currentColor" className="w-5 h-5">
                                     <path stroke-linecap="round" stroke-linejoin="round"
-                                          d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"/>
+                                        d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z" />
                                 </svg>
                         }
                         <div>{pressLike === true ? 2 : 1}</div>
@@ -84,7 +85,7 @@ export default function PostDetailPage() {
                 </div>
 
                 {/*작성자*/}
-                <PostWriter name={'작성자'} url={'../../images/sample22.jpeg'}/>
+                <PostWriter name={'작성자'} url={'../../images/sample22.jpeg'} />
 
                 {/*이전포스트*/}
                 <div className="w-full flex gap-2">
@@ -92,10 +93,10 @@ export default function PostDetailPage() {
                         <div className="flex w-1/2 bg-gray-200 cursor-pointer">
                             <div className="flex px-3 py-3.5 items-center gap-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                     stroke-width="1.3"
-                                     stroke="rgb(59 130 246)" className="w-9 h-9">
+                                    stroke-width="1.3"
+                                    stroke="rgb(59 130 246)" className="w-9 h-9">
                                     <path stroke-linecap="round" stroke-linejoin="round"
-                                          d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                                        d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg>
                                 <div>
                                     <div className="text-sm">이전 포스트</div>
@@ -111,10 +112,10 @@ export default function PostDetailPage() {
                                     <div className="font-bold">포스트이름입니다.</div>
                                 </div>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                     stroke-width="1.3"
-                                     stroke="rgb(59 130 246)" className="w-9 h-9">
+                                    stroke-width="1.3"
+                                    stroke="rgb(59 130 246)" className="w-9 h-9">
                                     <path stroke-linecap="round" stroke-linejoin="round"
-                                          d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                                        d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg>
                             </div>
                         </div> : <div></div>}
@@ -124,7 +125,7 @@ export default function PostDetailPage() {
                 <div className="w-full gap-2 flex flex-col">
                     <div className="font-bold">N개의 댓글</div>
                     <textarea
-                        className="w-full h-32 border border-gray-200 rounded-md py-1.5 px-2 resize-none focus:outline-none"/>
+                        className="w-full h-32 border border-gray-200 rounded-md py-1.5 px-2 resize-none focus:outline-none" />
                     <div className="flex justify-end">
                         <button
                             className="font-bold w-24 justify-center rounded-md py-1 px-2 flex bg-blue-500 hover:bg-blue-400 text-white">댓글
@@ -136,14 +137,14 @@ export default function PostDetailPage() {
                 {/*댓글목록*/}
                 <div className="mb-10">
                     {
-                        comments.map((item) => <CommentItem key={item.id} item={item.cmt}/>)
+                        comments.map((item) => <CommentItem key={item.id} item={item.cmt} />)
                     }
                 </div>
 
                 {/*추천게시글*/}
                 <div className="grid grid-cols-3 gap-4 mb-36">
                     {
-                        n.map((item) => <PostItem item={item}/>)
+                        n.map((item) => <PostItem item={item} />)
                     }
                 </div>
             </div>
