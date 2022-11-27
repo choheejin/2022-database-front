9ee1a6f5455e8cbbbefe18d733f65bf143a95d99
choheejin <hjcho9510@gmail.com>
2022-11-28 01:58:09 +0900

    나의 글 목록 완성

--- src/pages/post-page/PostListPage.js
+++ src/pages/post-page/PostListPage.js
@@ -1,14 +1,17 @@
-import {PostWriter} from "./components";
+import {MyPostItem, PostWriter} from "./components";
 
 export default function PostListPage() {
     return(
         <div className="w-full flex justify-center items-center">
-            <div className="w-[60%] flex flex-col gap-8">
-                <PostWriter name={'작성자'} url={'../../images/sample22.jpeg'} />
+            <div className="w-[60%] flex flex-col gap-8 mb-20">
+                <div className="mt-10">
+                    <PostWriter name={'작성자'} url={'../../images/sample22.jpeg'} />
+                </div>
 
+                <div className="mt-4">
+                    <MyPostItem />
+                </div>
             </div>
         </div>
-
-
     );
 }
--- src/pages/post-page/components/index.js
+++ src/pages/post-page/components/index.js
@@ -1,5 +1,6 @@
 import CommentItem from './CommentItem';
 import PostWriter from './PostWriter';
 import PostItem from './PostItem';
+import MyPostItem from "./MyPostItem";
 
-export {CommentItem, PostItem, PostWriter};
+export {CommentItem, PostItem, PostWriter, MyPostItem};
