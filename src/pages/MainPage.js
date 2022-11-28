f29e86ab54402ed930be8d81700967c42d3435bb
choheejin <hjcho9510@gmail.com>
2022-11-28 14:15:15 +0900

    카테고리 생성

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,6 +1,9 @@
 import PostItem from "./post-page/components/PostItem";
+import {useState} from "react";
 
 function MainPage() {
+    const [tab, setTab] = new useState(1);
+
     const n = [
     {
         id: 1,
@@ -32,10 +35,19 @@ function MainPage() {
 
     return(
         <div className="w-full flex justify-center">
-            <div className="w-[75%] mt-6 grid grid-cols-3 gap-5 items-center justify-center">
-                {
-                    n.map( (item) => <PostItem item={item} key={item.id}/> )
-                }
+            <div className="w-[75%] mt-3 ">
+                <div className="flex gap-2 mb-5 text-lg font-semibold mb-7">
+                    <div onClick={() => setTab(1)} className={`px-5 cursor-pointer select-none pb-1 ${tab === 1 ? 'text-blue-500 border-b-2 border-blue-500' : ''}`}>일상</div>
+                    <div onClick={() => setTab(2)} className={`px-5 cursor-pointer select-none pb-1 ${tab === 2 ? 'text-blue-500 border-b-2 border-blue-500' : ''}`}>고민</div>
+                    <div onClick={() => setTab(3)} className={`px-5 cursor-pointer select-none pb-1 ${tab === 3 ? 'text-blue-500 border-b-2 border-blue-500' : ''}`}>질문</div>
+                    <div onClick={() => setTab(4)} className={`px-5 cursor-pointer select-none pb-1  ${tab === 4 ? 'text-blue-500 border-b-2 border-blue-500' : ''}`}>최근 본 게시글</div>
+                </div>
+
+                <div className=" grid grid-cols-3 gap-5 items-center justify-center">
+                    {
+                        n.map( (item) => <PostItem item={item} key={item.id}/> )
+                    }
+                </div>
             </div>
         </div>
     );
