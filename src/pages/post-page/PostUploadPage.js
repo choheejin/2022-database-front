44eb1c721c9c0ca899482bff19428ff0caad1d03
yewon0804 <ehdnjs0804@gmail.com>
2022-12-14 12:15:21 +0900

    게시글 작성(Create) 구현
    CATTAS API로부터 랜덤 고양이 사진 받아옴
    백엔드와 연결되어 정상 작동

--- src/pages/post-page/PostUploadPage.js
+++ src/pages/post-page/PostUploadPage.js
@@ -1,15 +1,26 @@
 import '../../customAnimation.css';
-import { useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function PostUploadPage({ isVisible }) {
 
     isVisible(false);
 
     const [isModified, setIsModified] = useState(false);
-    const [isPublic, setIsPublic] = useState(true);
+    const [isPublic, setIsPublic] = useState(1);
 
-    // cat image
-    const sample = "https://cataas.com/cat";
+    // title, category, content
+    const [title, setTitle] = useState("");
+    const [category, setCategory] = useState(1); // article_type
+    const [content, setContent] = useState(""); // 본문
+
+    const [loading, setLoading] = useState(false);
+    const params = useParams();
+    const navigate = new useNavigate();
+
+    // cat image == thumbnail
+    const sample = "https://cataas.com//cat/y1sAMOXPczgTIVR1";
     const [mainCat, setMaincat] = useState(sample);
     const bg_url = "url(" + mainCat + ")";
 
@@ -27,8 +38,35 @@ export default function PostUploadPage({ isVisible }) {
     async function updateMainCat() {
         const newCat = await fetchCat();
         setMaincat(newCat);
-        console.log("냐옹");
+        console.log(newCat);
     }
+
+    const handleChangeCategory = (e) => {
+        setCategory(Number(e.target.value));
+        // console.log(e.target.value);
+    }
+
+    // const getContent = async () => {
+    //     // 수정하기 기능 구현 시 사용
+    //     return await axios.get(process.env.REACT_APP_API_URL + '/posts/write/update' + params.key);
+    // }
+
+    const postContent = () => {
+        // 넘겨줘야 할 것: title, thumbnail, content, is_public, user_id, type_id
+        axios.post(process.env.REACT_APP_API_URL + '/article/post', { title: title, thumbnail: mainCat, content: content, is_public: isPublic, user_id: 'dd', type_id: category }).then(response => {
+            setLoading(true);
+            navigate('/');
+        })
+    }
+
+    // useEffect(() => {
+    //     getContent().then(response => {
+    //         if (response.data.status === 200) {
+    //             setContent(response.data.response);
+    //             setLoading(false);
+    //         }
+    //     });
+    // }, [loading]);
 
     return (
         <div className="grid grid-cols-2 h-screen fadein">
@@ -47,28 +85,30 @@ export default function PostUploadPage({ isVisible }) {
             <form className="flex flex-col">
                 <input placeholder="제목"
                     name="title"
+                    onChange={(e) => { setTitle(e.target.value); }} value={title}
                     className="text-2xl font-bold focus:outline-none bg-transparent pl-8 pt-10 mb-3" />
 
                 <div className="flex w-full pl-8 mb-3 text-sm">
                     <div className="flex items-center mr-36">
                         <div className="pr-5">카테고리</div>
-                        <select className="focus:outline-none p-1 border border-gray-200 rounded-sm">
-                            <option>일상</option>
-                            <option>고민</option>
-                            <option>질문</option>
+                        <select onChange={handleChangeCategory} value={category} className="focus:outline-none p-1 border border-gray-200 rounded-sm">
+                            <option value="1">일상</option>
+                            <option value="2">고민</option>
+                            <option value="3">질문</option>
                         </select>
                     </div>
                     <div className="flex items-center text-sm">
                         <div className="pr-5">공개설정</div>
                         <div className="flex gap-4 items-center">
-                            <label ><input className="mr-1" type="radio" name="all" checked={isPublic} onChange={() => setIsPublic(!isPublic)} />전체공개</label>
-                            <label><input className="mr-1" type="radio" name="none" checked={!isPublic} onChange={() => setIsPublic(!isPublic)} />비공개</label>
+                            <label ><input className="mr-1" type="radio" name="all" checked={isPublic} onChange={() => setIsPublic(1)} />전체공개</label>
+                            <label><input className="mr-1" type="radio" name="none" checked={!isPublic} onChange={() => setIsPublic(0)} />비공개</label>
                         </div>
                     </div>
                 </div>
 
                 <textarea placeholder="본문을 입력해주세요"
                     name="content"
+                    onChange={(e) => { setContent(e.target.value); }} value={content}
                     className="bg-transparent focus:outline-none resize-none h-full pl-8 pt-1 mb-2.5" />
                 <div className="flex justify-between items-center bg-stone-700 py-2.5 px-3">
                     <a href="/" className="flex gap-1 text-white font-bold hover:bg-stone-500 rounded-md px-3.5 py-1.5">
@@ -79,7 +119,9 @@ export default function PostUploadPage({ isVisible }) {
                         </svg>
                         나가기
                     </a>
-                    <button className="bg-blue-500 hover:bg-blue-400 rounded-md text-white px-4 py-1.5 font-bold">
+                    <button onClick={postContent}
+                        type="button"
+                        className="bg-blue-500 hover:bg-blue-400 rounded-md text-white px-4 py-1.5 font-bold">
                         {isModified ? '수정하기' : '작성하기'}</button>
                 </div>
             </form>
