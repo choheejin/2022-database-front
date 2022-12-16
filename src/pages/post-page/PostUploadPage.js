ced687473ea7b8755fc70a8d0de78432a2962bd8
yewon0804 <ehdnjs0804@gmail.com>
2022-12-17 01:39:14 +0900

    featrue: 게시글 작성 시, 글자 수 노출(max, now)

--- src/pages/post-page/PostUploadPage.js
+++ src/pages/post-page/PostUploadPage.js
@@ -68,6 +68,36 @@ export default function PostUploadPage({ isVisible }) {
     //     });
     // }, [loading]);
 
+    // textarea 바이트 수 체크
+    function handlerContentKeyUp() {
+        const maxByte = 280;
+        const text_val = content; //입력한 문자
+        const text_len = content.length; //입력한 문자수
+
+        let totalByte = 0;
+        for (let i = 0; i < text_len; i++) {
+            const each_char = text_val.charAt(i);
+            const uni_char = escape(each_char); //유니코드 형식으로 변환
+            if (uni_char.length > 4) {
+                // 한글 : 2Byte
+                totalByte += 2;
+            } else {
+                // 영문,숫자,특수문자 : 1Byte
+                totalByte += 1;
+            }
+        }
+
+        if (totalByte > maxByte) {
+            document.getElementById("nowByte").innerText = totalByte;
+            document.getElementById("nowByte").style.color = "red";
+            document.getElementById("submit").disabled = 'disabled';
+        } else {
+            document.getElementById("nowByte").innerText = totalByte;
+            document.getElementById("nowByte").style.color = "green";
+            document.getElementById("submit").disabled = false;
+        }
+    }
+
     return (
         <div className="grid grid-cols-2 h-screen fadein">
             <div className="flex w-full bg-no-repeat bg-center bg-contain h-auto bg-stone-900"
@@ -109,7 +139,9 @@ export default function PostUploadPage({ isVisible }) {
                 <textarea placeholder="본문을 입력해주세요"
                     name="content"
                     onChange={(e) => { setContent(e.target.value); }} value={content}
+                    onKeyUp={(e) => { handlerContentKeyUp(); }}
                     className="bg-transparent focus:outline-none resize-none h-full pl-8 pt-1 mb-2.5" />
+                <sup className="p-4 text-sm">(<span id="nowByte">0</span>/280 bytes)</sup>
                 <div className="flex justify-between items-center bg-stone-700 py-2.5 px-3">
                     <a href="/" className="flex gap-1 text-white font-bold hover:bg-stone-500 rounded-md px-3.5 py-1.5">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
@@ -120,6 +152,7 @@ export default function PostUploadPage({ isVisible }) {
                         나가기
                     </a>
                     <button onClick={postContent}
+                        id="submit"
                         type="button"
                         className="bg-blue-500 hover:bg-blue-400 rounded-md text-white px-4 py-1.5 font-bold">
                         {isModified ? '수정하기' : '작성하기'}</button>
