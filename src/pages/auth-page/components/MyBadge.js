5c9b18daa15c64d035dfcce694b673e7f16721f4
Yeons <syg01103@naver.com>
2022-12-18 18:17:32 +0900

    뱃지 조건

--- src/pages/auth-page/components/MyBadge.js
+++ src/pages/auth-page/components/MyBadge.js
@@ -1,7 +1,63 @@
-export default function MyBadge (props) {
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+export default function MyBadge(props) {
+  // const [value, onChange] = useState(new Date());
+  const [attendanceList, setAttendanceList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getMyAttendance = async () => {
+    return await axios.get(
+      process.env.REACT_APP_API_URL +
+        "/mybadge/" +
+        localStorage.getItem("db-user_id")
+    );
+  };
+
+  useEffect(() => {
+    getMyAttendance().then((response) => {
+      setLoading(true);
+      setAttendanceList(response.data.response);
+    });
+  }, []);
+
   return (
-      <div className="rounded-full border border-gray-200 w-fit p-4">
-          <img className="w-16 h-16 object-cover object-center" src={process.env.REACT_APP_PUBLIC_URL+'/images/badge1.png'} />
+    <>
+      <div className="bg-gray-200 rounded-lg border-solid border-[1px] border-gray-400">
+        {loading ? (
+          <div className="flex p-3 gap-5">
+            {attendanceList.map((list, idx) => (
+              <div key={idx} className="">
+                <img
+                  className="w-16 object-center object-cover"
+                  src={process.env.REACT_APP_PUBLIC_URL + "/images/badge1.png"}
+                />
+                <label className="text-sm font-bold drop-shadow-lg">
+                  {list.attendance_date.slice(0, 10)}
+                </label>
+              </div>
+            ))}
+          </div>
+        ) : null}
       </div>
+    </>
   );
-};
+}
+
+// import React, { useState } from "react";
+// import Calendar from "react-calendar";
+// import moment from "moment/moment";
+// import "react-calendar/dist/Calendar.css"; // css import
+
+// export default function MyStamp(props) {
+//   const [value, onChange] = useState(new Date());n
+
+//   return (
+//     <div>
+//       <Calendar onChange={onChange} value={value} />
+//       <div className="text-gray-500 mt-4">
+//         {moment(value).format("YYYY년 MM월 DD일")}
+//       </div>
+//     </div>
+//   );
+// }
